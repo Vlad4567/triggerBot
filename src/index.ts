2c5ce7be39243a7db16e2b121e0dfd980e4835e4
alterproger <pensilproggertopg@gmail.com>
2024-09-11 13:17:05 +0300

    feat: add lib http

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import readline from "readline";
 
 import { IUserSchema } from "./models/userModel";
 import botActions from "./commands/botActions";
+import http from 'http';
 
 const { mongoURI } = databaseConfig;
 
@@ -26,6 +27,13 @@ export const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, World!\n");
+  })
+  .listen(process.env.PORT || 3000);
 
 mongoose.connect(mongoURI);
 
